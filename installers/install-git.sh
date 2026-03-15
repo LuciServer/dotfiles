4de980f4b63a28cc01790cf279cb2335b653568d
LuciKritZ <krishals.001@gmail.com>
2026-03-15 15:00:43 +0530

    bugfix: installer failing due to broken (

--- installers/install-git.sh
+++ installers/install-git.sh
@@ -13,19 +13,12 @@ sudo apt install -y git
 fi
 fi
 
-# Link gitconfig
+# Link git configuration
 ln -sf "$DOTFILES_DIR/git/.gitconfig" "$HOME/.gitconfig"
 
-echo "Checking SSH connectivity to GitHub..."
-
-if ssh -T [git@github.com](mailto:git@github.com) 2>&1 | grep -q "successfully authenticated"; then
-echo "SSH works. Enabling SSH URL rewrites..."
+echo "Enabling SSH URL rewrites..."
 
 git config --global url."[git@github.com](mailto:git@github.com):".insteadOf https://github.com/
 git config --global url."[git@gitlab.com](mailto:git@gitlab.com):".insteadOf https://gitlab.com/
-
-else
-echo "SSH not configured yet. Skipping URL rewrite."
-fi
 
 echo "Git configured."
